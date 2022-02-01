9de6f381e041969ce416e0385b7a1e6900be5be6
GraniteMask <ratnadeepastronomy@gmail.com>
2022-02-01 13:11:56 +0530

    LV completed

--- Solution by MATLAB/LVsolutionByMATLAB.m
+++ Solution by MATLAB/LVsolutionByMATLAB.m
@@ -1,4 +1,12 @@
 function xdot = LV(t, x) % t=time variable, x=state variable
 
+    alpha = 1.1;
+    beta = 0.4;
+    gamma = 0.4;
+    delta = 0.1;
+    
+    xdot = [alpha*x(1) - beta*x(1)*x(2)
+            delta*x(1)*x(2) - gamma*x(2)];
+    
 
 end
