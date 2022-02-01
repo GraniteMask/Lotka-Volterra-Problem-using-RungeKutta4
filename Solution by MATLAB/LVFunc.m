ca487b700d1df384a2182d88a868200369e160e6
GraniteMask <ratnadeepastronomy@gmail.com>
2022-02-01 20:10:57 +0530

    LV completed

---
+++ Solution by MATLAB/LVFunc.m
@@ -0,0 +1,12 @@
+function xdot = LV(~, x) % t=time variable but since we are not using it so replace it with ~, x=state variable
+
+    alpha = 1.1;
+    beta = 0.4;
+    gamma = 0.4;
+    delta = 0.1;
+    
+    xdot = [alpha*x(1) - beta*x(1)*x(2)
+            delta*x(1)*x(2) - gamma*x(2)];  %x(1) is the population of prey and x(2) is the population of predator
+    
+
+end
