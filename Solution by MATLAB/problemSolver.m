6cb6c22e1d11145e56c9cb4fc9ef53b62d337107
GraniteMask <ratnadeepastronomy@gmail.com>
2022-02-02 02:55:36 +0530

    solve using matlab completed

--- Solution by MATLAB/problemSolver.m
+++ Solution by MATLAB/problemSolver.m
@@ -20,7 +20,17 @@ dt = 0.01;
 %% PLOTTING THE RESULTS
 
 figure;
+
+subplot(1,2,1);  % It means this figure will be plotted in the first row and first column where there are total of 1 row and 2 column
 plot(t, x);
 legend('Preys', 'Predators');
 xlabel('Time(t)');
+title('t vs x')
+grid on;
+
+subplot(1,2,2);
+plot(x(1,:), x(2,:));
+xlabel('Preys');
+ylabel('Predators')
+title('Limit cycle behaviour')
 grid on;
