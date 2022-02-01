9134e3dcee91ec0d3441444e0be0b190b06621e4
GraniteMask <ratnadeepastronomy@gmail.com>
2022-02-02 03:12:44 +0530

    python solver

--- Solution by Python/problemSolver.py
+++ Solution by Python/problemSolver.py
@@ -9,7 +9,7 @@ Created on Wed Feb  2 02:56:21 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Defining the General Model
+# Defining the General Model
 
 def LV(x,params):
     
@@ -21,3 +21,18 @@ def LV(x,params):
     xdot = np.array([alpha*x[0] - beta*x[0]*x[1], delta*x[0]*x[1] - gamma*x[2]])
     
     return xdot
+
+# Defining the Runge Kutta 4 Method
+
+def RK4(f, x0, t0, tf, dt):
+    
+    t = np.arrange(t0, tf, dt)
+    nt = t.size(t0)
+    
+    nx = x0.size(t0)
+    x = np.zeroes((nx,nt))
+    
+    x[:,0] = x0
+    
+    for k in range(nt-1):
+    
